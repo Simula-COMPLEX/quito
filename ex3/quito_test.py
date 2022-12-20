677ae08a8d15c59d69aa8c63a50d828fd5fa147c
qiqihannah <784960656@qq.com>
2022-12-20 15:15:17 +0100

    Update quito_test.py

--- ex3/quito_test.py
+++ ex3/quito_test.py
@@ -1,4 +1,14 @@
-from quito import quito
+#from quito import quito
 
-quito("/home/xinyi/quito/ex3/as.ini")
+#quito("/home/xinyi/quito/ex3/as.ini")
+
+
+from quito.quito_coverage import quito
+import time
+start = time.time()
+i = 0
+quito('/home/xinyi/quito/ex3/as.ini')
+end = time.time()
+
+print(end - start)
 
